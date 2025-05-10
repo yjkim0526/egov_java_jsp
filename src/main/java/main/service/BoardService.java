3e08d3f1c7f8c420062190c10978caa9ca6e997f
yjkim0526 <koiforever0526@gmail.com>
2025-05-10 23:06:30 +0900

    docs 생성

--- src/main/java/main/service/BoardService.java
+++ src/main/java/main/service/BoardService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface BoardService {
-	List<?> selectBoardList(Map<String, Object> map);
+    List<?> selectBoardList(Map<String, Object> map);
     int selectBoardTotalCount(Map<String, Object> map);
     void insertBoard(BoardVO vo);
     BoardVO selectBoardDetail(int unq);
